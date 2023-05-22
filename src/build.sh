db537af6911942899d33320a5feb82c8ab72f798
Hendricks27 <wenjinzhang95@gmail.com>
2023-05-22 13:40:34 -0500

    update

--- src/build.sh
+++ src/build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-tag="V0.0.1"
+tag="V0.0.7"
 
 # --platform linux/amd64
 docker build -t juanruguo/pycallingcards:$tag -t juanruguo/pycallingcards:latest ./
 
-# docker push juanruguo/pycallingcards:$tag
-# docker push juanruguo/pycallingcards:latest
+docker push juanruguo/pycallingcards:$tag
+docker push juanruguo/pycallingcards:latest
 
 docker run -p 10981:10981 juanruguo/pycallingcards
 
--- src/install_env.sh
+++ src/install_env.sh
@@ -10,9 +10,13 @@ pip install -r requirements.txt
 mkdir /homer/
 mv /code/configureHomer.pl /homer/configureHomer.pl
 cd /homer/
+
 perl ./configureHomer.pl -install
+# perl ./configureHomer.pl -install hg38
+# perl ./configureHomer.pl -install mm10
+perl ./configureHomer.pl -install sacCer3
 
-
+rm build.sh Dockerfile requirements.txt install_env.sh
 
 
 
